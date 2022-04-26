76a6a8ec010bf3908a8942356a4caf3d93471622
Syafiqm <syafiq4000@gmail.com>
2022-04-27 03:38:22 +0800

    ninth_commit

--- handle_calculator.php
+++ handle_calculator.php
@@ -15,3 +15,33 @@ B<html>
 </body>
 
 </html>
+if (isset($source)) {
+
+if ($source == 'calculator.html') {
+
+if ((is_numeric (Squantity))) {
+
+Stotal = (Squantity ~ Sprice) ($taxrate +1);
+
+Stotal = number format ($total, 2);
+
+echo "You are purchasing <b>" .$_POST["quantity"]
+
+"</b> widgets at a cost of <b> ". $_POST["price"]
+
+"</b> each. With tax the total comes to <b>". $total . "</b>.\n";
+
+}else{
+
+echo "<p><b>Please enter a valid quantity to purchase!</b></p>";
+
+}
+
+}else{
+
+echo "<p><b>You have accessed this page inappropriately!</b></p>";
+
+}else{
+
+echo "<p><b>You have accessed this page inappropriately!</b></p>";
+}
